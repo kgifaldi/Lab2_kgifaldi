9e6be630a81dc2814df7d63f90698d1fc3c456bc
Kyle <kgifaldi@nd.edu>
2017-03-22 02:55:43 -0400

    lab5

--- app/src/main/java/com/cs60333/kgifaldi/lab2_kgifaldi/MainActivity.java
+++ app/src/main/java/com/cs60333/kgifaldi/lab2_kgifaldi/MainActivity.java
@@ -3,11 +3,20 @@ package com.cs60333.kgifaldi.lab2_kgifaldi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuBuilder;
+import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.content.Context;
+// TODO why wont these import???
+import android.support.design.widget.Snackbar;
 
+import android.support.design.widget.CoordinatorLayout;
 
 import java.util.ArrayList;
 
@@ -21,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Toolbar...
+
+        Toolbar t = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(t);
+        t.setTitle("ND Athletics");
 
         //Team teams = new String[]{"Ohio State", "Florida State", "Wake Forest", "Boston College", "North Carolina State", "Georgia Tech", "North Virginia", "Chicago State"};
 /*
@@ -82,4 +96,96 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    public boolean onCreateOptionsMenu (Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public String gameSchedule(){
+        StringBuilder str = new StringBuilder();
+        for(int i = 0; i < al.size(); i++) {
+            str.append((al.get(i).getTeamName()));
+            str.append((al.get(i).getGameDate()));
+        }
+        return str.toString();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int res_id = item.getItemId();
+
+        if (res_id == R.id.share) {
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(android.content.Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra("android.content.Intent.EXTRA_SUBJECT", "BasketBall Matches");
+            shareIntent.putExtra("android.content.Intent.EXTRA_TEXT", gameSchedule());
+            startActivity(Intent.createChooser(shareIntent), "Share via");
+        }
+
+        else if (res_id == R.id.sync) {
+// Snackbar with Try Again action
+            Coordinatorlayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorlayout);
+
+            SnackBar snackbar = SnackBar.make(coordinatorLayout, "Sync is not yet implemented", Snackbar.LENGTH_LONG);
+
+            snackbar.setAction("Try Again", new View.OnClickListener() {
+
+                @Override
+
+                public void onClick(View v) {
+
+                    SnackBar.make(coordinatorLayout, "Wait for the next few labs. Thank you for your patience", Snackbar.LENGTH_LONG).show();
+
+                }
+
+            });
+
+            snackbar.show();
+
+        }
+
+        else if (res_id == R.id.settings) {
+            @Override
+
+            public void onCreateContextMenu (ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+                super.onCreateContextMenu(menu, v, menuInfo);
+ //  TODO   !!!!!!!!!!        //  registerforContextMenu((View) getViewById(R.id.settings))
+
+                MenuInflater menuInflater = getMenuInflater();
+                menuInflater.inflate(R.menu.floating_contextual_menu, menu);
+            }
+
+            @Override
+
+            public boolean onContextItemSelected(MenuItem item) {
+
+                int item_id = item.getItemId();
+
+                if (item_id == R.id.women) {
+
+// to be implemented later
+
+                }
+                else if (item_id == R.id.men){
+
+                }
+                else if (item_id == R.id.on){
+
+                }
+                else if (item_id == R.id.off){
+
+                }
+                return false;
+
+            }
+
+
+        }
+        return true;
+    }
+
 }
